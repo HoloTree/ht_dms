52ffbcb7e0bcaf7e6a228a0a6005e39fbbf82544
Josh Pollock <jpollock412@gmail.com>
2014-06-22 20:03:48 -0400

    correct swapped organization preview/full view

--- ht_dms/ui/views/partials/organization_preview.php
+++ ht_dms/ui/views/partials/organization_preview.php
@@ -1,39 +1,4 @@
-<div class="organization organization-single">
+<div class="organization organization-preview">
 	<h3>{@post_title}</h3>
 	<p>Description: {@description}</p>
-	[if groups]
-		<div class="organizations-groups">
-			<h5>Groups</h5>
-			<ul>
-				[each groups]
-				<li>{@post_title}</li>
-				[/each]
-			</ul>
-		</div>
-	[/if]
-	[if decisions]
-		<div class="organizations-decisions">
-			<h5>Decisions</h5>
-			<ul>
-				[each decisions]
-				<li>{@post_title}
-					<ul>
-						<li>{@decision_status}</li>
-					</ul>
-				</li>
-				[/each]
-			</ul>
-		</div>
-	[if members]
-		<div class="organizations-members">
-			<h5>Members</h5>
-			<ul>
-				[each members]
-				<li>{@display_name}</li>
-				[/each]
-			</ul>
-		</div>
-	[/if]
-
-
 </div>
