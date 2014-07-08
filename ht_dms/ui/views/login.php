e7d872b8c54de678ee002b6c88cfca76e2787f4b
Josh Pollock <jpollock412@gmail.com>
2014-07-08 01:04:26 -0400

    Header/footer for login fix

--- ht_dms/ui/views/login.php
+++ ht_dms/ui/views/login.php
@@ -1,5 +1,25 @@
 <?php
-htdms_theme_header();
+$stylesheet = get_stylesheet();
+if ( $stylesheet === 'ht_dms_theme' ) {
+	htdms_theme_header();
+}
+elseif ( $stylesheet === 'app_starter' ) {
+	app_starter_header();
+}
+else {
+	get_header();
+}
+
 echo holotree_dms_ui()->login()->output();
-htdms_theme_footer();
+
+if ( $stylesheet === 'ht_dms_theme' ) {
+	htdms_theme_footer();
+}
+elseif ( $stylesheet === 'app_starter' ) {
+	app_starter_footer();
+}
+else {
+get_footer();
+}
+
 ?>
