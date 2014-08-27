ee239558dfa1cb0f91baf7cf60b698493dddcfbe
Josh Pollock <jpollock412@gmail.com>
2014-08-26 23:03:05 -0400

    Organization, better handling for not a member.

--- ht_dms/ui/views/organization-single.php
+++ ht_dms/ui/views/organization-single.php
@@ -29,34 +29,39 @@ if ( $org_class->is_member( $id, $uID, $obj ) || $org_class->open_access( $id, $
 
 	$tabs = array (
 
-	array (
-		'label'   => __( 'My Groups In Organization', 'holotree' ),
-		'content' => $ui->views()->users_groups( null, $uID, $id ),
-	),
-	array (
-		'label'   => __( 'Public Groups In Organization', 'holotree' ),
-		'content' => $ui->views()->public_groups( null, $id ),
-	),
-	array (
-		'label'   => __( 'Assigned Tasks In This Organization', 'holotree' ),
-		'content' => $ui->views()->assigned_tasks( null, $uID, $id ),
-	),
+		array (
+			'label'   => __( 'My Groups In Organization', 'holotree' ),
+			'content' => $ui->views()->users_groups( null, $uID, $id ),
+		),
+		array (
+			'label'   => __( 'Public Groups In Organization', 'holotree' ),
+			'content' => $ui->views()->public_groups( null, $id ),
+		),
+		array (
+			'label'   => __( 'Assigned Tasks In This Organization', 'holotree' ),
+			'content' => $ui->views()->assigned_tasks( null, $uID, $id ),
+		),
 
-	array (
-		'label'   => __( 'New Group In Organization', 'holotree' ),
-		'content' => $ui->add_modify()->new_group(  $id, $uID ),
-	),
-	array(
-		'label'		=> __( 'Edit Organization', 'holotree' ),
-		'content'	=> $ui->add_modify()->edit_organization( $id, $uID, $obj ),
-	),
+		array (
+			'label'   => __( 'New Group In Organization', 'holotree' ),
+			'content' => $ui->add_modify()->new_group(  $id, $uID ),
+		),
+		array(
+			'label'		=> __( 'Edit Organization', 'holotree' ),
+			'content'	=> $ui->add_modify()->edit_organization( $id, $uID, $obj ),
+		),
 
 
 	);
 
-	return $ui->elements()->output_container( $tabs );
+
 
 }
 else {
-	return 'NEED A NO ACCESS SOMETHING!?';
+	$tabs = array(
+		'label'		=> get_post_title( $id ),
+		'content'	=> __( 'You must be a member of this organization to view it\'s content', 'holotree' ),
+	);
 }
+
+return $ui->elements()->output_container( $tabs );
