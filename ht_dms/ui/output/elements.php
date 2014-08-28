fce45c53bcb424048f37872c9edc3cbcd42c9bff
Josh Pollock <jpollock412@gmail.com>
2014-08-28 14:52:52 -0400

    Fix current_url() function to make work

--- ht_dms/ui/output/elements.php
+++ ht_dms/ui/output/elements.php
@@ -313,9 +313,9 @@ class elements {
 		}
 		$pageURL .= "://";
 		if ($_SERVER["SERVER_PORT"] != "80") {
-			$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+			$pageURL .= $_SERVER["SERVER_ADDR"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
 		} else {
-			$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+			$pageURL .= $_SERVER["SERVER_ADDR"].$_SERVER["REQUEST_URI"];
 		}
 		return $pageURL;
 	}
