HoloTree/ht_dms

commit 9b5c06c9757c14c813034e2d5729a66f6ff98f00

Decision shouldn't go to completed when PropMod is accepted by original mod proposer
close #22

ht_dms/decision.php
@@ -668,10 +668,14 @@ class decision extends dms {
 		//get ID of original item and create seprate object for that.
 		$original_obj = $this->item( $original_id  );
 
+		/*
+		 * @todo put this back?
+		 * @see https://github.com/HoloTree/ht_dms/issues/31
+		 * @see https://github.com/HoloTree/ht_dms/issues/22
 		//proceed directly to acceptance if accepting user is the original
 		if ( (int) $original_obj->field( 'post_author' ) === ( $uID = (int) get_current_user_id()  ) ) {
 			$make_mod = true;
-		}
+		}*/
 		else {
 			$id = $this->accept( $id, $uID, $obj );
 			if ( $this->has_consent( $id, $obj ) ) {

commit 29c73e4ce523b2cba2dec9da7bb2518c73d44a8c

fix syntax error introduced by 9b5c06c

ht_dms/decision.php
@@ -675,14 +675,14 @@ class decision extends dms {
 		//proceed directly to acceptance if accepting user is the original
 		if ( (int) $original_obj->field( 'post_author' ) === ( $uID = (int) get_current_user_id()  ) ) {
 			$make_mod = true;
-		}*/
-		else {
+		}
+		else {*/
 			$id = $this->accept( $id, $uID, $obj );
 			if ( $this->has_consent( $id, $obj ) ) {
 				$make_mod = true;
 			}
 
-		}
+		//}
 
 		if ( $make_mod ) {
 			$this->make_modification( $id, $original_id, $obj, $uID, $original_obj );