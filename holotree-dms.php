840657c2f50f7982e74e435fd7843cbd8a9a4759
Josh Pollock <jpollock412@gmail.com>
2014-09-05 14:14:23 -0400

    Make version 0.0.3.a.1

--- holotree-dms.php
+++ holotree-dms.php
@@ -1,6 +1,7 @@
 <?php
 /*
 Plugin Name: HoloTree Decision Making System
+Version: 0.0.3.a.1
 License: GPL v2 or Later
 */
 
@@ -39,7 +40,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
  * 
  * @since 0.0.1
  */
-define( 'HT_DMS_VERSION', '0.0.1' );
+define( 'HT_DMS_VERSION', '0.0.3.a.1' );
 define( 'HT_DMS_DB_VERSION', '1' );
 define( 'HT_DMS_SLUG', plugin_basename( __FILE__ ) );
 define( 'HT_DMS_ROOT_URL', plugin_dir_url( __FILE__ ) );
