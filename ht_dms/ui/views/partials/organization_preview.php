e84aa43862bf7243726a3fc030cc81f0c5cf2c58
Josh Pollock <jpollock412@gmail.com>
2014-09-27 20:50:32 -0400

    members face view for organizations #66

--- ht_dms/ui/views/partials/organization_preview.php
+++ ht_dms/ui/views/partials/organization_preview.php
@@ -1,5 +1,6 @@
 <div class="organization organization-preview">
 	<h3><a href="{@permalink}" class="organization-link button" organization="{@id}" title="View organization" id="organization-link">{{organization}}{@post_title}</a></h3>
 	<p class="description">Description: {@description}</p>
+	<li><div class="dms-members-load" dms_type="organization" dms_id="{@id}" id="group-{@id}"></div></li>
 </div>
 
