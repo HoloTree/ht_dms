9dc52b0dc53e0f574d753737e92170ca9dadb381
Josh Pollock <jpollock412@gmail.com>
2014-11-17 17:56:31 -0500

    icons/translation domain clean up in propose-modify view

--- ht_dms/ui/views/propose-change.php
+++ ht_dms/ui/views/propose-change.php
@@ -7,21 +7,27 @@ $class = ht_dms_decision_class();
 $ui = ht_dms_ui();
 $uID = get_current_user_id();
 
-$id = pods_v( 'dms_id' );
+$id = pods_v_sanitized( 'dms_id' );
 
 //@todo check if current user is a member of group
 
 $tabs = array(
 	array(
-		'label' => ht_dms_add_icon( __( 'Propose Modification', 'ht_dms' ), array( 'new', 'modification' ) ),
+		'label' => ht_dms_add_icon( __( 'Propose Modification', 'ht-dms' ), array( 'new', 'modification' ) ),
 		'content' => $ui->add_modify()->modify_decision( $id, null, $uID ),
 	),
 	array(
-		'label' => ht_dms_add_icon( __( 'Decision Being Modified' ), 'decision' ),
-		'content' => $ui->views()->decision( null, $id ),
+		'label' => ht_dms_add_icon( __( 'Decision Being Modified', 'ht-dms' ), 'decision' ),
+		'content' => $ui->views()->decision(
+			array(
+				'id' => $id,
+				'preview' => true,
+				'return' => 'template'
+			)
+		),
 	),
 	array(
-		'label' => ht_dms_add_icon( __( 'Discussion From Original Decision' ), 'discussion' ),
+		'label' => ht_dms_add_icon( __( 'Discussion From Original Decision', 'ht-dms' ), 'discussion' ),
 		'content' 	=> $ui->elements()->discussion( $id, 5, true ),
 	),
 );
