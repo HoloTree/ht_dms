6867c36478f5cc59a5f01ec57842cb7e2ea733c4
Josh Pollock <jpollock412@gmail.com>
2014-11-21 19:40:37 -0500

    inline docs

--- ht_dms/helper/registration/organization/check.php
+++ ht_dms/helper/registration/organization/check.php
@@ -13,12 +13,30 @@ namespace ht_dms\helper\registration\organization;
 
 class check implements \Filter_Hook_SubscriberInterface  {
 
-
+	/**
+	 * Register the filter for this class
+	 *
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return array
+	 */
 	public static  function  get_filters() {
 		return array( 'json_authentication_errors' => array( 'run_checks', 99, 1 ) );
 
 	}
 
+	/**
+	 * Check if code is valid and allow use of endpoint if so.
+	 *
+	 * @uses json_authentication_errors filter
+	 *
+	 * @since 0.1.0
+	 *
+	 * @param $pass
+	 *
+	 * @return null|\WP_Error|\WP_JSON_ResponseInterface
+	 */
 	public function run_checks( $pass ) {
 		if ( is_null( $pass ) ) {
 			$code = \ht_dms\api\internal\route::get_post_param( 'invite' );
@@ -39,6 +57,13 @@ class check implements \Filter_Hook_SubscriberInterface  {
 
 	}
 
+	/**
+	 * Error to return if code fail.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return \WP_Error|\WP_JSON_ResponseInterface
+	 */
 	private function error() {
 		$message = __( 'Invite code for organization not validated.', 'ht-dms' );
 		$response = json_ensure_response( json_encode( array( 'message' => $message ) ) );
@@ -46,6 +71,7 @@ class check implements \Filter_Hook_SubscriberInterface  {
 		$response->header( 'Location', json_url( '/pods/' . HT_DMS_ORGANIZATION_POD_NAME ) );
 
 		return $response;
+
 	}
 
 
