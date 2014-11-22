9611af44c8858f1b923cda90f3442c156c4d9820
Josh Pollock <jpollock412@gmail.com>
2014-11-21 20:53:39 -0500

    skip internal api cache for 'new_organization_code', 'reload_consensus', 'reload_membership'

--- ht_dms/api/internal/route.php
+++ ht_dms/api/internal/route.php
@@ -247,7 +247,7 @@ class route implements \Action_Hook_SubscriberInterface {
 	 * @return bool|string
 	 */
 	private static function cache_key( $params, $action ) {
-		if ( ! HT_DEV_MODE && is_array( $params ) && ! apply_filters( 'ht_dms_internal_api_skip_cache', false, $action, $params ) ) {
+		if ( ! HT_DEV_MODE && is_array( $params ) && !( in_array( $action, array( 'new_organization_code', 'reload_consensus', 'reload_membership' ) ) ) && ! apply_filters( 'ht_dms_internal_api_skip_cache', false, $action, $params ) ) {
 			global $cuID;
 			$cache_key = array_merge( $params, array( $action, $cuID  )  );
 			$cache_key = implode( $cache_key, '=' );
