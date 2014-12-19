061607a71299608015f6b4640a531c19b88f1395
Josh Pollock <jpollock412@gmail.com>
2014-12-19 11:56:59 -0500

    rename views dir to templates
    close #148

---
+++ ht_dms/api/internal/actions/consensus_details.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @TODO What this does.
+ *
+ * @package   @TODO
+ * @author    Josh Pollock <Josh@JoshPress.net>
+ * @license   GPL-2.0+
+ * @link      
+ * @copyright 2014 Josh Pollock
+ */
+
+namespace ht_dms\api\internal\actions;
+
+
+class consensus_details {
+
+} 
