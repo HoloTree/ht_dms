3cfd2616d4fb3124d8c165d57d5f185902c216ef
Josh Pollock <jpollock412@gmail.com>
2014-12-19 13:06:26 -0500

    Add, but not implement interface for internal api actions #157

--- ht_dms/api/internal/actions/action.php
+++ ht_dms/api/internal/actions/action.php
@@ -14,4 +14,33 @@ namespace ht_dms\api\internal\actions;
 
 interface action {
 
+	/**
+	 * Will be called by API router. Must return the response.
+	 *
+	 * @param array $params An array of params, defined by self::args()
+	 *
+	 * @return mixed
+	 */
+	public static function act( $params );
+
+	/**
+	 * Params for this route
+	 *
+	 * Add an array of the names of GET or POST vars to pass into self::act()
+	 *
+	 * @return array
+	 */
+	public static function args();
+
+	/**
+	 * Define if this action should use GET or POST.
+	 *
+	 * This method should either be:
+	 *
+	 * return "GET"; or return "POST";
+	 *
+	 * @return string
+	 */
+	public static function method();
+
 } 
