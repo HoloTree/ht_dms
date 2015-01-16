5f70fd308c4afb5acc716a0fddbb614ab9494884
Josh Pollock <jpollock412@gmail.com>
2015-01-16 18:32:44 -0500

    rm wp-api client #150

--- holotree-dms.php
+++ holotree-dms.php
@@ -167,12 +167,7 @@ class HoloTree_DMS {
 			if ( ! function_exists( 'json_get_url_prefix' ) ) {
 				return;
 			}
-
-			wp_enqueue_script( 'wp-api-js', plugins_url( '/js/wp-api.min.js', __FILE__ ), array( 'jquery', 'underscore', 'backbone' ), '1.0', true );
-
-			$settings = array( 'root' => home_url( json_get_url_prefix() ), 'nonce' => wp_create_nonce( 'wp_json' ) );
-
-			wp_localize_script( 'wp-api-js', 'WP_API_Settings', $settings );
+			
 
 		}
 
